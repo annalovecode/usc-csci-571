58523b7f7e117f6c4551fee2658ae200bc04c326
Rochak Gupta <rochak84@gmail.com>
2020-09-26 19:09:57 -0700

    Minor changes in HW6 frontend

--- hw6/static/index.js
+++ hw6/static/index.js
@@ -1,55 +1,50 @@
-const App = (function () {
+const App = (() => {
     "use strict";
 
     let searchInput = null;
     let searchButton = null;
     let clearButton = null;
-
     let searchQuery = null;
-
     let resultSuccess = null;
     let resultError = null;
-
     let selectedSection = null;
-
     let companyOutlook = 'company-outlook';
     let stockSummary = 'stock-summary';
     let charts = 'charts';
     let latestNews = 'latest-news';
-
     const resultSuccessTab = {};
     const resultSuccessContent = {};
-
     const selectedTabClass = 'selected-tab';
     const hideClass = 'hide';
 
-    function hide(element) {
+    const hide = (element) => {
         element.classList.add(hideClass);
     }
 
-    function show(element) {
+    const show = (element) => {
         element.classList.remove(hideClass);
     }
 
-    function selectTab(tabElement) {
+    const selectTab = (tabElement) => {
         tabElement.classList.add(selectedTabClass);
     }
 
-    function unselectTab(tabElement) {
+    const unselectTab = (tabElement) => {
         tabElement.classList.remove(selectedTabClass);
     }
 
-    function showing(element) {
-        return !(element.classList.contains(hideClass));
-    }
+    const showing = (element) => !(element.classList.contains(hideClass));
 
-    function buildCompanyOutlook(data) {
-        function buildRow(key, value) {
-            return `<div>
-                        <div>${key}</div>
-                        <div><p>${value}</p></div>
-                    </div>`;
-        }
+    const floatFormatter = (value) => value.toFixed(2);
+
+    const buildCompanyOutlook = (data) => {
+        const buildRow = (key, value) =>
+            `<div>
+                <div>${key}</div>
+                <div>
+                    <p>${value}</p>
+                </div>
+            </div>`;
 
         resultSuccessContent[companyOutlook].innerHTML =
             buildRow('Company Name', data['companyName']) +
@@ -59,15 +54,17 @@ const App = (function () {
             buildRow('Description', data['description']);
     }
 
-    function buildStockSummary(data) {
-        function buildRow(key, value) {
-            return `<div>
-                        <div>${key}</div>
-                        <div><p>${value}</p></div>
-                    </div>`;
-        }
+    const buildStockSummary = (data) => {
+        const buildRow = (key, value) =>
+            `<div>
+                <div>${key}</div>
+                <div>
+                    <p>${value}</p>
+                </div>
+            </div>`;
 
-        function buildCellWithImage(value, isPercentage = false) {
+
+        const buildNumberCellWithImage = (value, isPercentage = false) => {
             let cell = value.toString();
             if (isPercentage) {
                 cell += '%';
@@ -87,20 +84,20 @@ const App = (function () {
         resultSuccessContent[stockSummary].innerHTML =
             buildRow('Stock Ticker Symbol', data['stockTickerSymbol']) +
             buildRow('Trading Day', data['tradingDay']) +
-            buildRow('Previous Closing Price', data['previousClosingPrice']) +
-            buildRow('Opening Price', data['openingPrice']) +
-            buildRow('High Price', data['highPrice']) +
-            buildRow('Low Price', data['lowPrice']) +
-            buildRow('Last Price', data['lastPrice']) +
-            buildRow('Change', buildCellWithImage(data['change'])) +
-            buildRow('Change Percent', buildCellWithImage(data['changePercent'], true)) +
+            buildRow('Previous Closing Price', floatFormatter(data['previousClosingPrice'])) +
+            buildRow('Opening Price', floatFormatter(data['openingPrice'])) +
+            buildRow('High Price', floatFormatter(data['highPrice'])) +
+            buildRow('Low Price', floatFormatter(data['lowPrice'])) +
+            buildRow('Last Price', floatFormatter(data['lastPrice'])) +
+            buildRow('Change', buildNumberCellWithImage(floatFormatter(data['change']))) +
+            buildRow('Change Percent', buildNumberCellWithImage(floatFormatter(data['changePercent']), true)) +
             buildRow('Number of Shares Traded', data['numberOfSharesTraded']);
     }
 
-    function buildCharts(data) {
+    const buildCharts = (data) => {
         const stockPrices = [];
         const volumes = [];
-        data.forEach(function ({ date, stockPrice, volume }) {
+        data.forEach(({ date, stockPrice, volume }) => {
             const epochDate = Date.parse(date);
             stockPrices.push([epochDate, stockPrice]);
             volumes.push([epochDate, volume]);
@@ -167,6 +164,10 @@ const App = (function () {
                 spacingRight: 15
             },
 
+            tooltip: {
+                xDateFormat: '%A, %b %e, %Y'
+            },
+
             series: [{
                 name: ticker,
                 type: 'area',
@@ -198,42 +199,41 @@ const App = (function () {
         });
     }
 
-    function buildLatestNews(data) {
-        function buildNews(news) {
-            return `<div>
-                        <div style="background-image: url(${news['image']});">
-                        </div>
-                        <div>
-                            <div>${news['title']}</div>
-                            <div>Published Date: ${news['date']}</div>
-                            <div>
-                                <a href="${news['linkToOriginalPost']}" target="_blank">See Original Post</a>
-                            </div>
-                        </div>
-                    </div>`
-        }
+    const buildLatestNews = (data) => {
+        const buildNews = (news) =>
+            `<div>
+                <div style="background-image: url(${news['image']});">
+                </div>
+                <div>
+                    <div>${news['title']}</div>
+                    <div>Published Date:  ${news['date']}</div>
+                    <div>
+                        <a href="${news['linkToOriginalPost']}" target="_blank">See Original Post</a>
+                    </div>
+                </div>
+            </div>`;
 
         let innerHTML = "";
-        data.forEach(function (news) {
+        data.forEach((news) => {
             innerHTML += buildNews(news);
         });
 
         resultSuccessContent[latestNews].innerHTML = innerHTML;
     }
 
-    function buildSectionError(section, message) {
+    const buildSectionError = (section, message) => {
         resultSuccessContent[section].innerHTML =
             `<p>
                 ${message}
              </p>`
     }
 
-    function hideSection(section) {
+    const hideSection = (section) => {
         unselectTab(resultSuccessTab[section]);
         hide(resultSuccessContent[section]);
     }
 
-    function showSection(section) {
+    const showSection = (section) => {
         if (selectedSection !== null) {
             hideSection(selectedSection);
         }
@@ -242,35 +242,33 @@ const App = (function () {
         show(resultSuccessContent[section]);
     }
 
-    function showResultSuccess() {
+    const showResultSuccess = () => {
         show(resultSuccess);
         hide(resultError);
     }
 
-    function showResultError(message) {
+    const showResultError = (message) => {
         hide(resultSuccess);
         resultError.innerHTML = message;
         show(resultError);
     }
 
-    function fetchSection(ticker, section) {
+    const fetchSection = (ticker, section) => {
         class CheckedError extends Error {
             constructor(message) {
                 super(message);
             }
         }
 
-        return fetch(`${section}/${ticker}`).then(function (response) {
-            return response.json().then(function (data) {
-                if (response.status === 200) {
-                    return data;
-                } else if (response.status === 404) {
-                    throw new CheckedError(data['message']);
-                } else {
-                    throw new CheckedError("Internal server error");
-                }
-            })
-        }).catch(function (error) {
+        return fetch(`${section}/${ticker}`).then((response) => response.json().then((data) => {
+            if (response.status === 200) {
+                return data;
+            } else if (response.status === 404) {
+                throw new CheckedError(data['message']);
+            } else {
+                throw new CheckedError("Internal server error");
+            }
+        })).catch((error) => {
             if (error instanceof CheckedError) {
                 throw error.message;
             }
@@ -278,12 +276,10 @@ const App = (function () {
         })
     }
 
-    function search(ticker) {
-        function isResolved(response) {
-            return response.status === 'fulfilled';
-        }
+    const search = (ticker) => {
+        const isResolved = (response) => response.status === 'fulfilled';
 
-        function buildSection(section, response, builder) {
+        const buildSection = (section, response, builder) => {
             if (isResolved(response)) {
                 builder(response.value);
             } else {
@@ -296,7 +292,7 @@ const App = (function () {
             fetchSection(ticker, stockSummary),
             fetchSection(ticker, charts),
             fetchSection(ticker, latestNews)
-        ]).then(function ([companyOutlookResponse, stockSummaryResponse, chartsResponse, latestNewsResponse]) {
+        ]).then(([companyOutlookResponse, stockSummaryResponse, chartsResponse, latestNewsResponse]) => {
             if (!isResolved(companyOutlookResponse)) {
                 showResultError(companyOutlookResponse.reason);
             } else if (!isResolved(stockSummaryResponse)) {
@@ -312,8 +308,8 @@ const App = (function () {
         })
     }
 
-    function searchButtonClickHandler(event) {
-        const ticker = searchInput.value;
+    const searchButtonClickHandler = (event) => {
+        const ticker = searchInput.value.toUpperCase();
         if (ticker.length > 0) {
             event.preventDefault();
             if (ticker !== searchQuery) {
@@ -326,12 +322,12 @@ const App = (function () {
         }
     }
 
-    function clearInput() {
+    const clearInput = () => {
         searchInput.value = '';
         searchQuery = null;
     }
 
-    function clearResult() {
+    const clearResult = () => {
         if (selectedSection !== null) {
             hideSection(selectedSection);
             selectedSection = null;
@@ -340,27 +336,28 @@ const App = (function () {
         hide(resultError);
     }
 
-    function clearButtonClickHandler() {
+    const clearButtonClickHandler = (event) => {
+        event.preventDefault();
         clearInput();
         clearResult();
     }
 
-    function tabClickHandler(event) {
+    const tabClickHandler = (event) => {
         const section = (event.target.id).substring(11);
         showSection(section);
     }
 
-    function init() {
+    const init = () => {
         searchInput = document.querySelector('#search-field-input > input');
 
         searchButton = document.querySelector('#search-button > input');
         searchButton.addEventListener("click", searchButtonClickHandler);
 
-        clearButton = document.getElementById('clear-button');
+        clearButton = document.querySelector('#clear-button > input');
         clearButton.addEventListener("click", clearButtonClickHandler);
 
         resultSuccess = document.getElementById('result-success');
-        [companyOutlook, stockSummary, charts, latestNews].forEach(function (section) {
+        [companyOutlook, stockSummary, charts, latestNews].forEach((section) => {
             resultSuccessTab[section] = document.getElementById(`result-tab-${section}`);
             resultSuccessTab[section].addEventListener("click", tabClickHandler);
             resultSuccessContent[section] = document.getElementById(`result-${section}`);
