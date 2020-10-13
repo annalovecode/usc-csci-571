aa355fd88eed0b1e79bb0efc192ade902cba79ec
Rochak Gupta <rochak84@gmail.com>
2020-10-13 01:50:23 -0700

    Serve client from server in HW8

--- hw8/server/src/index.ts
+++ hw8/server/src/index.ts
@@ -1,11 +1,32 @@
 import express from 'express';
+import expressWinston from 'express-winston';
+import winston from 'winston';
+import * as path from 'path';
 
 const app: express.Application = express();
 const port = 3000;
 
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console()
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json(),
+    winston.format.prettyPrint()
+  ),
+  meta: true,
+  msg: 'HTTP {{req.method}} {{req.url}}',
+  expressFormat: true,
+  colorize: true,
+  ignoreRoute: (req, res) => false
+}));
+
+app.use(express.static(path.join(__dirname, '../../client/dist')));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
